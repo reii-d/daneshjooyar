e6f65b5756dc4a41ddc38dffc40382b5edbd52a8
mohsen-norouzi237 <mnorouzi2018@gmail.com>
2024-06-03 03:24:56 +0330

    login signup prof

---
+++ flutt/lib/pages/admin_login.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class admin_login extends StatefulWidget {
+  const admin_login({super.key});
+
+  @override
+  State<admin_login> createState() => _admin_loginState();
+}
+
+class _admin_loginState extends State<admin_login> {
+  TextEditingController adminIdControl = TextEditingController();
+  bool _isObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[400],
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Login!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Enter your admin Id",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: adminIdControl,
+                decoration: InputDecoration(
+                  labelText: "Admin Id",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscured ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscured = !_isObscured;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: _isObscured,
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.cyan,
+                    foregroundColor: Colors.white,
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text("Login"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- src/Course.java
+++ src/Course.java
@@ -140,3 +140,5 @@ public class Course {
         return highest;
     }
 }
+
+
